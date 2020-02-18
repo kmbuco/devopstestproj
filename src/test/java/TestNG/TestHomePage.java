7f94c1f8160f89ff8acb73a81d676e77654a3b5b
Kelvin K. Mbuco <kmbuco@gmail.com>
2020-02-18 13:12:42 +0300

    Run Home Test
    Added option to run the test in the headless state.

--- src/test/java/TestNG/TestHomePage.java
+++ src/test/java/TestNG/TestHomePage.java
@@ -20,7 +20,7 @@ public class TestHomePage {
 		System.setProperty("webdriver.chrome.driver","/var/lib/jenkins/workspace/chromedriver");
 		//System.setProperty("webdriver.chrome.driver","E:\\development\\STSWorkspace\\workspace5\\devopstestproj\\chromedriver.exe");
 		ChromeOptions chromeOptions= new ChromeOptions();
-    	//chromeOptions.addArguments("--headless");
+    	chromeOptions.addArguments("--headless");
 		driver = new ChromeDriver(chromeOptions);
 		driver.get("http://34.69.4.133:8082/");
 		driver.manage().window().maximize();
